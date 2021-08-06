a6882e07e43311a99796a4762c2862acc64b7e0e
Felipe Hernandez <72527112+juanfelipehdezm@users.noreply.github.com>
2021-08-05 20:51:11 -0500

    Add files via upload
    small changes

--- trivia/answers.js
+++ trivia/answers.js
@@ -3,15 +3,19 @@ document.addEventListener("DOMContentLoaded",
 function()
 {
     //when correct answer is clicked, chage color to green
+    //when the button with the correct answer is clicked create a anonymus function
     let correct = document.querySelector(".correct");
     correct.addEventListener("click",
     function() 
         {
+            //change the color background of .correct html correct class
             correct.style.background = "green";
+            //select the html element with the id = #feedback and write in it (innerHTML)
             document.querySelector("#feedback1").innerHTML = "Correct!";
         });
 
     //when any incorrect answer is clicked, chand color to red
+    //the selectorall return an array with all the elements selected that have the .incorrect class
     let incorrects = document.querySelectorAll(".incorrect");
     for (let i = 0; i < incorrects.length; i++) 
     {
